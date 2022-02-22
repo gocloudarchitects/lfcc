ca1ae01582639a0e348449a80a2dd1c335ac1b9a
nathancurry <nathan.curry@redis.com>
2022-02-21 20:29:19 -0500

    set openstack version to stable/xena

--- openstack/lab/04-prepare-kolla.sh
+++ openstack/lab/04-prepare-kolla.sh
@@ -14,10 +14,12 @@ source ~/venv/kolla/bin/activate
 pip install -U pip
 pip install 'ansible==5.*'
 
-# Install the latest version of Kolla-Ansible
+# Install the 'Xena' release
+pip install git+https://opendev.org/openstack/kolla-ansible@stable/xena || pip install git+https://github.com/openstack/kolla-ansible@stable/xena
+# Install the latest (possibly development) version of Kolla-Ansible
 # pip install git+https://opendev.org/openstack/kolla-ansible@master || pip install git+https://github.com/openstack/kolla-ansible@master
 # Install known-good commit from 02/09/2022
-pip install git+https://github.com/openstack/kolla-ansible@556d979930bdb24c3ee46413f7b77584017b9206
+# pip install git+https://github.com/openstack/kolla-ansible@556d979930bdb24c3ee46413f7b77584017b9206
 
 pip install python-openstackclient -c https://releases.openstack.org/constraints/upper/master || pip install git+https://github.com/openstack/python-openstackclient
 
