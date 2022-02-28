a38361e5893bf5ac2ce677055ed4a5e5a9ca6f8e
nathancurry <nathan.curry@redis.com>
2022-02-28 18:35:03 -0500

    clean up cruft

---
+++ terraform/stack-tenants/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
+# Configure the OpenStack Provider
+# Credentials from course lab, please provide your own if different
+provider "openstack" {
+  user_name   = var.stack_credentials.user_name
+  tenant_name = var.stack_credentials.tenant_name
+  password    = var.stack_credentials.password
+  auth_url    = var.stack_endpoint.auth_url
+  region      = var.stack_endpoint.region
+}
--- terraform/stack-tenants/variables.tf
+++ terraform/stack-tenants/variables.tf
@@ -1,81 +1,8 @@
-#
-# variables.tf initializes and describes input variables
-#
 
 
 variable "tenants" {
   description = "list of tenants"
   type = set(string)
-}
-
-
-variable "provider_network" {
-  description = "CIDR of physical access network"
-    default = {
-    cidr   = "192.168.6.0/24"
-    pool_start = "192.168.6.100"
-    pool_end   = "192.168.6.199"
-  }
-  type = object({
-    cidr = string
-    pool_start = string
-    pool_end = string
-    })
-}
-
-variable "admin_cidr" {
-  description = "CIDR of admin test network"
-  default = "172.168.0.0/24"
-}
-
-
-
-##### IMAGES AND FLAVORS ####
-
-variable "default_image" {
-  description = "Default VM image made available to tenants"
-  default = {
-    name = "cirros"
-    url = "https://github.com/cirros-dev/cirros/releases/download/0.5.2/cirros-0.5.2-x86_64-disk.img"
-    version_tag = "0.5.3"
-  }
-  type = map(string)
-}
-
-variable "flavors" {
-  description = "Public flavors"
-  type = list(object({
-    name = string
-    ram = number
-    vcpus = number
-    disk = number
-  }))
-  default = [
-  {
-    name = "m1.tiny"
-    ram = 512
-    vcpus = 1
-    disk = 1
-  },
-  {
-    name = "m1.small"
-    ram = 1024
-    vcpus = 1
-    disk = 5
-  },
-  {
-    name = "m1.medium"
-    ram = 2048
-    vcpus = 2
-    disk = 10
-  },
-  {
-    name = "m1.large"
-    ram = 4096
-    vcpus = 2
-    disk = 40
-  }
-  ]
 }
 
 
