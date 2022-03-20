b3dc33bd06c38f1487fc7ab6d17a1ff96afd717a
nathancurry <nathan.curry@redis.com>
2022-03-20 12:11:06 -0400

    fix script

--- exercises/files/breakboot.sh
+++ exercises/files/breakboot.sh
@@ -14,8 +14,10 @@ then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
 elif [[ $REPLY =~ ^[Yy]$ ]]
   then
+  echo "GRUB_TIMEOUT=30" >> /etc/default/grub.d/99-timeout.cfg
+  grub-mkconfig -o /boot/grub/grub.cfg &>/dev/null
   usermod root --password `date +%s | md5sum -z | awk '{print $1}'` &>/dev/null 
   usermod vagrant --password `date +%s | md5sum -z | awk '{print $1}'` &>/dev/null
-  deluser vagrant &>/dev/null
+  touch /etc/ssh/sshd_not_to_be_run &>/dev/null
   reboot
 fi
